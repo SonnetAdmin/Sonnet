7d3f1f3a109c881056acf8966f8bc8caa3f2e7e9
SonnetAdmin <admin@sonnetsoft.com>
2026-07-16 11:15:15 +0100

    Refactor TestPage for improved readability and logging
    Refactor TestPage class to use constants for selectors and replace print statements with logger.

--- src/test/java/sonnet/po/TestPage.java
+++ src/test/java/sonnet/po/TestPage.java
@@ -1,14 +1,27 @@
-package org.sonnet.po;
+package sonnet.po;
 
-import org.sonnet.CommonPage;
+import sonnet.CommonPage;
+import sonnet.dto.Interests;
+import sonnet.logger.Logger;
 
-import static org.sonnet.CommonPageInterface.Trait.*;
-import static org.sonnet.utils.Print.print;
+import static sonnet.utils.StringUtils.isTrue;
+import static sonnet.CommonPageInterface.Trait.*;
+
 
 public class TestPage extends CommonPage {
-    public TestPage() {
+    private final String COOKIES = "#ez-accept-all";
+    private final String EMAIL = "#email";
+    private final String USERNAME = "#username";
+    private final String PASSWORD = "#password";
+    private final String SPORTS = "#sports";
+    private final String MUSIC = "#music";
+    private final String READING = "#reading";
+    private final String TRAVEL = "#travel";
+    private final String SUBMIT = "#submit-btn";
+    private final String SUBMITTED = "#form-result";
 
-    }
+    Logger logger = new Logger();
+    public TestPage() {}
 
     public void openBrowser() {
         open();
@@ -16,50 +29,60 @@ public class TestPage extends CommonPage {
 
     public void navigate(String url) {
         go(url);
-        expect("#ez-accept-all").click();
+        expect(COOKIES).click();
     }
 
     public void enterName(String name) {
-        focus("#username").compose(name);
+        focus(USERNAME).compose(name);
     }
 
     public void enterEmail(String email) {
-        focus("#email").compose(email);
-
+        focus(EMAIL).compose(email);
     }
 
     public void enterPassword(String password) {
-        focus("#password").compose(password);
+        focus(PASSWORD).compose(password);
     }
 
     public void checkGet() {
-        print(get(TEXT));
-        print(get(TAG));
-        print(get(XPOS));
-        print(get(YPOS));
-        print(get(WIDTH));
-        print(get(HEIGHT));
-        print(get(ENABLED));
-        print(get(VISIBLE));
+        logger.log(get(TEXT));
+        logger.log(get(TAG));
+        logger.log(get(XPOS));
+        logger.log(get(YPOS));
+        logger.log(get(WIDTH));
+        logger.log(get(HEIGHT));
+        logger.log(get(ENABLED));
+        logger.log(get(VISIBLE));
     }
 
     public void selectCountry(String country) {
         focus("#country").click();
         sleep(2);
         collect("option");
-        printElements();
-        choose(2);
-        printElement();
+        choose(country);
         click();
     }
 
     public void clickSubmit() {
-        focus("#submit-btn").click();
+        focus(SUBMIT).click();
     }
 
     public void verifyLogin() {
-
+        focus(SUBMITTED);
     }
 
-
+    public void selectInterests(Interests interests) {
+        if (isTrue(interests.getSports())) {
+            focus(SPORTS).click();
+        }
+        if (isTrue(interests.getMusic())) {
+            focus(MUSIC).click();
+        }
+        if(isTrue(interests.getReading())) {
+            focus(READING).click();
+        }
+        if (isTrue(interests.getTravel())) {
+            focus(TRAVEL).click();
+        }
+    }
 }
