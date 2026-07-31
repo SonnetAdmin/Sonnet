bb8066b1851dd8d6ea9600b6905fc6e4a0471052
SonnetAdmin <admin@sonnetsoft.com>
2026-07-31 16:10:23 +0100

    Refactor CommonPage class and add interface

--- src/main/java/sonnet/CommonPage.java
+++ src/main/java/sonnet/CommonPage.java
@@ -1,176 +1,53 @@
 package sonnet;
 
-import com.microsoft.playwright.*;
-import com.microsoft.playwright.options.BoundingBox;
-import sonnet.CommonPageInterface;
-import sonnet.utils.Logger;
-
-import java.util.List;
-
-import static org.sonnet.utils.Print.*;
-
-public class CommonPage extends TargetFactory implements CommonPageInterface {
-
-    private Page page;
-    private Locator currentElement;
-    private List<Locator> currentElements;
-    private final Logger logger = new Logger();  // This is the logger from sonnet.utils
-
-    @Override
-    public CommonPage open() {
-        logger.log("open()\t", "Chrome");
-        try {
-            Playwright playwright = Playwright.create();
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-            page = browser.newPage();
-        } catch (PlaywrightException pe) {
-            print("Playwright Exception" + pe.getMessage());
-
-        }
-        return this;
+public interface CommonPageInterface {
+    enum Action{
+        BACKSPACE,
+        DOWN,
+        ENTER,
+        HOME,
+        LEFT,
+        RIGHT,
+        SPACE,
+        TAB,
+        UP;
     }
 
-    @Override
-    public CommonPage go(String url) {
-        logger.log("go()\t", url);
-        page.navigate(url);
-        return this;
+    enum Trait{
+        ENABLED,
+        HEIGHT,
+        SELECTED,
+        TAG,
+        TEXT,
+        VISIBLE,
+        WIDTH,
+        XPOS,
+        YPOS;
+    }
+    enum DialogAction{
+        OK,
+        CANCEL;
     }
 
-    @Override
-    public CommonPage focus(Target target) {
-        logger.log("focus()\t",target.getType().toString(), target.getValue());
-        switch (target.getType()) {
-            case PLACEHOLDER:
-                currentElement = page.getByPlaceholder(target.getSelector());
-                break;
+    CommonPage click();
+    CommonPage choose(String name);
+    CommonPage choose(int index);
+    CommonPage collect(String locator);
+    CommonPage compose(String text);
+    CommonPage compose(Action action);
+    CommonPage focus(Target target);
 
-            case LABEL:
-                currentElement = page.getByLabel(target.getValue(), new Page.GetByLabelOptions().setExact(true));
-                break;
+    CommonPage collect(Target target);
 
-            case ALT:
-                currentElement = page.getByAltText(target.getSelector());
-                break;
+    String get(Trait trait);
+    CommonPage go(String url);
+    CommonPage open();
+    CommonPage sleep(int milliseconds);
+    CommonPage submit();
+    String getTitle();
+    CommonPage upload(Target target, String filePath);
+    CommonPage dialog(DialogAction action);
+    void store(String key, String value);
+    String retrieve(String key);
 
-            case TITLE:
-                currentElement = page.getByTitle(target.getSelector());
-                break;
-
-            default: {
-                List<Locator> elements = page.locator(target.getSelector()).all();
-                currentElement = elements.getFirst();
-            }
-        }
-        return this;
-    }
-
-    @Override
-    public CommonPage sleep(int milliseconds) {
-        logger.log("sleep()\t", "Milliseconds ",String.valueOf(milliseconds));
-        try {
-            Thread.sleep(milliseconds * 1000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        return this;
-    }
-
-    @Override
-    public CommonPage click() {
-        logger.log("click()\t");
-        currentElement.click();
-        return this;
-    }
-
-    @Override
-    public CommonPage choose(String name) {
-        logger.log("choose()\t", "String ", name);
-        for (Locator listElement : currentElements) {
-            if (listElement.textContent().contains(name)) {
-                currentElement = listElement;
-                print("found");
-                break;
-            }
-        }
-        return this;
-    }
-
-    @Override
-    public CommonPage choose(int index) {
-        logger.log("choose()\t", "Int ", String.valueOf(index));
-        currentElement = currentElements.get(index);
-        return this;
-    }
-
-    @Override
-    public CommonPage compose(String text) {
-        logger.log("compose()", "Text ", text);
-        currentElement.fill(text);
-        return this;
-    }
-
-    @Override
-    public CommonPage compose(Action action) {
-        logger.log("compose()", "Action ", action.toString());
-        currentElement.fill(action.toString());
-        return this;
-    }
-
-    // TODO review
-    @Override
-    public CommonPage submit() {
-        logger.log("submit()\t");
-        return this;
-    }
-
-    @Override
-    public CommonPage collect(String locator) {
-        logger.log("collect()", locator);
-        currentElements = currentElement.locator(locator).all();
-        return this;
-        //TODO FIX LATER
-    }
-
-    @Override
-    public String get(Trait trait) {
-        logger.log("get()\t", trait.name());
-        return switch (trait) {
-            case TEXT -> currentElement.textContent();
-            case TAG -> currentElement.evaluate("el => el.tagName").toString();
-            case XPOS -> String.valueOf(currentElement.boundingBox().x);
-            case YPOS -> String.valueOf(currentElement.boundingBox().y);
-            case WIDTH -> String.valueOf(currentElement.boundingBox().width);
-            case HEIGHT -> String.valueOf(currentElement.boundingBox().height);
-            case ENABLED -> String.valueOf(currentElement.isEnabled());
-            case VISIBLE -> String.valueOf(currentElement.isVisible());
-            case SELECTED -> String.valueOf(currentElement.isChecked());
-
-        };
-    }
-
-    //////////////////////////
-    /// DIAGNOSTIC METHODS ///
-    //////////////////////////
-
-    public CommonPage printElements() {
-        logger.log("print elements");
-        printPartition();
-        for (Locator listElement : currentElements) {
-            print(listElement.textContent());
-        }
-        return this;
-    }
-
-    public CommonPage printElement() {
-        logger.log("print element");
-        BoundingBox box = currentElement.boundingBox();
-        print("Tag: \t\t" + currentElement.evaluate("el => el.tagName").toString());
-        print("Text: \t\t" + currentElement.textContent());
-        print("Enabled: \t" + currentElement.isEnabled());
-        print("Visible: \t" + currentElement.isVisible());
-        print("Location: \t " + box.x + " " + box.y);
-        print("Dimensions: \t" + box.width + " " + box.height);
-        return this;
-    }
 }
